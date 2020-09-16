aefc706a9f76adcece5806dd8bee3cbb84cd12ed
RazzleJazzled <robertjgassner@gmail.com>
2020-09-16 14:27:21 -0700

    init commit

---
+++ Develop/models/game.js
@@ -0,0 +1,44 @@
+module.exports = function(sequelize, DataTypes) {
+    const Game = sequelize.define("Game", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: false,
+        },
+        genre: {
+            type: DataTypes.ENUM,
+            values: ['Action', 'Adventure', 'Fighting', 'Platformer', 'Racing', 'RPG','Shooter', 'Sports', 'Strategy', 'Survival/Horror']
+        },
+        publisher: {
+            type: DataTypes.STRING,
+            
+        },
+        systemPreference:{
+            type: DataTypes.ENUM,
+            values: ['Android', 'iOS', 'MacOS', 'PC', 'Nintendo Switch', 'Nintendo Wii', 'Nintendo WiiU', 'Playstation 3','Playstation 4', 'Xbox ONE', 'XBOX 360' ]
+        },
+
+        rating: {
+            type: DataTypes.ENUM,
+            values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            allowNull: false
+        },
+
+        wishlist: {
+            type: DataTypes.BOOLEAN,
+            default: true
+        },
+
+        playing: {
+            type: DataTypes.BOOLEAN,
+            default: false
+        },
+
+        beaten: {
+            type: DataTypes.BOOLEAN,
+            default: false
+        }
+
+    })
+    return Game;
+    }
