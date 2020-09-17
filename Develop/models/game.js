d74af8022e0f9d31a14085642070a5f28f89e813
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-17 11:18:48 -0700

    updating file structure

--- Develop/models/game.js
+++ Develop/models/game.js
@@ -18,10 +18,15 @@ module.exports = function(sequelize, DataTypes) {
             values: ['Android', 'iOS', 'MacOS', 'PC', 'Nintendo Switch', 'Nintendo Wii', 'Nintendo WiiU', 'Playstation 3','Playstation 4', 'Xbox ONE', 'XBOX 360' ]
         },
 
-        rating: {
-            type: DataTypes.ENUM,
-            values: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+        // rating: {
+        //     type: DataTypes.ENUM,
+        //     values: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
            
+        // },
+        rating: {
+          type: DataTypes.INTEGER({
+            isBetween: (1,10),
+          })
         },
 
         wishlist: {
